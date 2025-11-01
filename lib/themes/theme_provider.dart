edbf6addd1ad533db06cb6f2341112e16099ae06
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-11-01 20:43:36 +0700

    refactor: theme provider now utilizes shared_preferences

--- lib/themes/theme_provider.dart
+++ lib/themes/theme_provider.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
+  static const _prefKey = 'isDarkMode';
+
   bool _isDark = false;
   bool get isDark => _isDark;
   ThemeMode get mode => _isDark ? ThemeMode.dark : ThemeMode.light;
 
+  ThemeProvider() {
+    _loadFromPrefs();
+  }
+
+  Future<void> _loadFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDark = prefs.getBool(_prefKey) ?? false;
+    notifyListeners();
+  }
+
+  Future<void> _saveToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_prefKey, _isDark);
+  }
+
   void toggle() {
     _isDark = !_isDark;
     notifyListeners();
+    _saveToPrefs();
   }
 
   void setDark(bool value) {
     if (_isDark == value) return;
     _isDark = value;
     notifyListeners();
+    _saveToPrefs();
   }
 }
