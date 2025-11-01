91a10dc16b1d6022664bd720c6d54af27482a22c
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-11-01 20:34:55 +0700

    feat: add light and dark mode configurations

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: const ColorScheme.dark(
+    surface: Color.fromRGBO(18, 18, 18, 1),
+    primary: Color.fromARGB(255, 56, 56, 56),
+    secondary: Color.fromARGB(255, 151, 154, 180),
+    inversePrimary: Colors.white,
+  ),
+);
---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: const ColorScheme.light(
+    surface: Color.fromRGBO(237, 242, 244, 1),
+    primary: Color.fromARGB(255, 202, 214, 235),
+    secondary: Color.fromARGB(255, 151, 154, 180),
+    inversePrimary: Colors.black,
+  ),
+);
